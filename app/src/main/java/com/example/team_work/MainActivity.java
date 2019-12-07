d2f04bb05d47c8e42a49139f4312155336151517
Tim_cjh <1424715994@qq.com>
2019-12-07 21:02:47 +0800

    test_cjh

--- app/src/main/java/com/example/team_work/MainActivity.java
+++ app/src/main/java/com/example/team_work/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.team_work;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a="jiojioio";
+        Toast.makeText(MainActivity.this,a,Toast.LENGTH_SHORT).show();
     }
 }
