73c4e5c6f4b90ff61b96feabe1f3b23df2a87b64
Tim_cjh <1424715994@qq.com>
2020-01-17 18:19:03 +0800

    test_cjh

--- app/src/main/java/com/example/team_work/MainActivity.java
+++ app/src/main/java/com/example/team_work/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a="jiojioio";
+        String b="完成了在这打上名字：";
         Toast.makeText(MainActivity.this,a,Toast.LENGTH_SHORT).show();
     }
 }
